a3f7c3d4551da5bda91be909f44f7be12c79c231
Cpuhouse <devel@cpuhouse.com.br>
2020-08-17 19:46:12 -0300

    Import repository

---
+++ assets/components/example.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="alert alert-success d-flex flex-row">
+    <div class="logo px-3">
+      <img src="https://vuejs.org/images/logo.png" alt="vue logo" height="80" />
+    </div>
+    <div>
+      <h1>It worked!</h1>
+      <p class="lead">Vue is loaded successfully.</p>
+    </div>
+  </div>
+</template>
